6520a76bc52707f02d419154db4aee737ca48416
HMMP karunarathne <karunarathnamihiri429@gmail.com>
2026-02-02 10:05:57 +0530

    Normalize line endings

---
+++ src/main/java/com/babyshop/hellobaby/HellobabyApplication.java
@@ -0,0 +1,13 @@
+package com.babyshop.hellobaby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobabyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellobabyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/babyshop/hellobaby/HellobabyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babyshop.hellobaby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellobabyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
